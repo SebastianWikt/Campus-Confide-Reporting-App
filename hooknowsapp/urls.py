65583889171255b5ca654a145db174a57c382751
Khyaif Qaiser <kaffyqaiser@Kaffys-MacBook-Pro.local>
2024-02-22 15:24:38 -0500

    local setup with sqlite

--- hooknowsapp/urls.py
+++ hooknowsapp/urls.py
@@ -1,10 +1,9 @@
-import users
 from django.contrib import admin
 from django.urls import path, include
 from hooknowsapp import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # Updated to 'login_view'
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('home/', views.home, name='home'),  # Ensure this points to the 'home' view
+    path('home/', views.home, name='home'),
 ]
