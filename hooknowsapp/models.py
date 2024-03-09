21669327f27228e5267868feb7ce8b5a3bf7db29
ZichengHe-Susan <bgc4bx@virginia.edu>
2024-03-08 21:35:24 -0500

    built part of the dashboard and added the skeleton of question and answer model

--- hooknowsapp/models.py
+++ hooknowsapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, max_length=200)
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
