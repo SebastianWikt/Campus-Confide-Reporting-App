766a60ec6fb53bd06c54d1b4f1ee77ea6c52ffb3
aimam24 <imamanjala24@gmail.com>
2024-03-10 00:40:20 -0500

    added user model

--- hooknowsapp/models.py
+++ hooknowsapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -9,8 +10,14 @@ class Question(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True, max_length=200)
 
+
 class Answer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     answer = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_staff = models.BooleanField(default=False)
