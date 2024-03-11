3ca9b48479fdfb135f7442c8620565d43c069dfc
Sebastian Wiktorowicz <114835029+SebastianWikt@users.noreply.github.com>
2024-03-11 17:35:01 -0400

    created model for report and in models

--- hooknowsapp/models.py
+++ hooknowsapp/models.py
@@ -24,3 +24,6 @@ class User(models.Model):
 
 class Report(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, max_length=200)
