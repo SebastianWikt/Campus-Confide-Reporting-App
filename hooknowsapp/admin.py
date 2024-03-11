ac9d72378cf63a6cbc3f64ca41a847bb1d65bfcb
Sebastian Wiktorowicz <114835029+SebastianWikt@users.noreply.github.com>
2024-03-11 15:13:15 -0400

    changed html file names and added view reports html

--- hooknowsapp/admin.py
+++ hooknowsapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Question, Answer, User
+from .models import Report, Answer, User
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Report)
 admin.site.register(Answer)
 admin.site.register(User)
