afdac97a207edf0f9552a0ad6bc5ec99e08c2114
Khyaif Qaiser <kaffyqaiser@Kaffys-MacBook-Pro.local>
2024-03-30 17:34:15 -0400

    migrations deployment

--- hooknowsapp/migrations/0003_report_user_alter_answer_user_alter_question_user_and_more.py
+++ hooknowsapp/migrations/0003_report_user_alter_answer_user_alter_question_user_and_more.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('hooknowsapp', '0004_report_file.py'),
+        ('hooknowsapp', '0004_report_file'),
     ]
 
     operations = [
