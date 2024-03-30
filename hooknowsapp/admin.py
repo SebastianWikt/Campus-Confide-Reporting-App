1fbc15eb5b572ad5d257d6dbafa2d1eefbe92009
Khyaif Qaiser <kaffyqaiser@Kaffys-MacBook-Pro.local>
2024-03-30 16:53:25 -0400

    heroku fix

--- hooknowsapp/admin.py
+++ hooknowsapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Question, Answer, User, Report
+from .models import Question, Answer, User, Report, AdminNote
 
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Report)
+admin.site.register(AdminNote)
