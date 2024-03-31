9bb6ee41d3bc6374a4ada9c0f4b8a98846da804f
Khyaif Qaiser <kaffyqaiser@Kaffys-MBP.home>
2024-03-31 12:51:57 -0400

    heroku changes

--- hooknowsapp/admin.py
+++ hooknowsapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Question, Answer, User, Report, AdminNote
+from .models import Question, Answer, User, Report, AdminNote, Notification
 
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Report)
 admin.site.register(AdminNote)
+admin.site.register(Notification)
